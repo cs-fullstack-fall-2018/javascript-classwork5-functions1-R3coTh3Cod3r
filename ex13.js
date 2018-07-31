2cc1fb592bed84c284fc59fd25b6feb6cd0ed158
Mareco Nabors <naborsmarico99@gmail.com>
2018-07-31 11:41:20 -0500

    functions classwork5

---
+++ ex13.js
@@ -0,0 +1,17 @@
+function main() {
+   alertHello();
+  alertWorld();
+}
+
+
+
+function alertHello() {
+    alert("Hello")
+}
+
+function alertWorld() {
+    alert("World")
+
+}
+
+main();
---
+++ ex14.js
@@ -0,0 +1,6 @@
+function main() {
+    var favoriteTeacher = "Kevin";
+    alert(favoriteTeacher)
+}
+
+main();
